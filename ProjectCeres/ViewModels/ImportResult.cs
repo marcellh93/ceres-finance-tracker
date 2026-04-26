19cd33bf3b8ff72cb94f0908ec07e715e2bbfde3
Marcell Hernández <marcelljesus1218@gmail.com>
2026-04-27 00:04:12 +0200

    feat: add RowsStaged to ImportResult and ImportSummaryViewModel

--- ProjectCeres/ViewModels/ImportResult.cs
+++ ProjectCeres/ViewModels/ImportResult.cs
@@ -5,6 +5,7 @@ public class ImportResult
     public int RowsImported    { get; set; }
     public int RowsReconciled  { get; set; }
     public int RowsFlagged     { get; set; }
+    public int RowsStaged      { get; set; }
     public int RowsFailed      { get; set; }
     public List<string> Errors { get; set; } = [];
 }
