0cfa5a964cabfdc0d549d7367062350093be2380
Marcell Hernández <marcelljesus1218@gmail.com>
2026-05-09 14:57:46 +0200

    feat(auth): EF migration AddCreatedAtToApplicationUser

---
+++ ProjectCeres/Migrations/20260509125640_AddCreatedAtToApplicationUser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectCeres.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedAtToApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "current_timestamp");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AspNetUsers");
+        }
+    }
+}
