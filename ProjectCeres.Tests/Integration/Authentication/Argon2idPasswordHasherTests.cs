245ad2a46cdee988c95e1454f15c51ce22e32a15
Marcell Hernández <marcelljesus1218@gmail.com>
2026-05-09 04:20:36 +0200

    test(auth): Argon2idPasswordHasher rehash + dummy timing tests

--- ProjectCeres.Tests/Integration/Authentication/Argon2idPasswordHasherTests.cs
+++ ProjectCeres.Tests/Integration/Authentication/Argon2idPasswordHasherTests.cs
@@ -35,4 +35,38 @@ public class Argon2idPasswordHasherTests
 
         result.Should().Be(PasswordVerificationResult.Failed);
     }
+
+    [Fact]
+    public void VerifyHashedPassword_returns_SuccessRehashNeeded_when_stored_params_below_target()
+    {
+        var weak = new Argon2idPasswordHasher(Options.Create(new Argon2idOptions
+        {
+            MemorySizeKb = 4096, Iterations = 1, Parallelism = 1
+        }));
+        var user = new ApplicationUser();
+        var weakHash = weak.HashPassword(user, "abc12345");
+
+        var prod = new Argon2idPasswordHasher(Options.Create(new Argon2idOptions()));
+        var result = prod.VerifyHashedPassword(user, weakHash, "abc12345");
+
+        result.Should().Be(PasswordVerificationResult.SuccessRehashNeeded);
+    }
+
+    [Fact]
+    public void RunDummyHash_completes_within_an_order_of_magnitude_of_real_hash()
+    {
+        var hasher = new Argon2idPasswordHasher(Options.Create(new Argon2idOptions()));
+        var user = new ApplicationUser();
+
+        var realStart = DateTime.UtcNow;
+        var hash = hasher.HashPassword(user, "real-password-here");
+        hasher.VerifyHashedPassword(user, hash, "real-password-here");
+        var realMs = (DateTime.UtcNow - realStart).TotalMilliseconds;
+
+        var dummyStart = DateTime.UtcNow;
+        hasher.RunDummyHash();
+        var dummyMs = (DateTime.UtcNow - dummyStart).TotalMilliseconds;
+
+        (dummyMs / Math.Max(realMs, 1.0)).Should().BeInRange(0.1, 10.0);
+    }
 }
