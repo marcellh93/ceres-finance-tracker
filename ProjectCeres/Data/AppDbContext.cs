8c8d8c8f4ebcf85d369f6794e9ebc46a1ca80b5c
Marcell Hernández <marcelljesus1218@gmail.com>
2026-05-09 04:17:11 +0200

    feat(auth): AppDbContext inherits IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>

--- ProjectCeres/Data/AppDbContext.cs
+++ ProjectCeres/Data/AppDbContext.cs
@@ -1,10 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ProjectCeres.Common;
 using ProjectCeres.Models;
 
 namespace ProjectCeres.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
