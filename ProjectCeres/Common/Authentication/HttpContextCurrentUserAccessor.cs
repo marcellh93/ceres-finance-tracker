f3bc61de0af51360d24c9e9fa43860be6fb3d0d4
Marcell Hernández <marcelljesus1218@gmail.com>
2026-05-12 01:02:52 +0200

    refactor(stage-7): primary constructor on HttpContextCurrentUserAccessor

--- ProjectCeres/Common/Authentication/HttpContextCurrentUserAccessor.cs
+++ ProjectCeres/Common/Authentication/HttpContextCurrentUserAccessor.cs
@@ -4,25 +4,16 @@ using ProjectCeres.Common;
 
 namespace ProjectCeres.Common.Authentication;
 
-public sealed class HttpContextCurrentUserAccessor : ICurrentUserAccessor
+public sealed class HttpContextCurrentUserAccessor(IHttpContextAccessor http, IUserScope scope) : ICurrentUserAccessor
 {
-    private readonly IHttpContextAccessor _http;
-    private readonly IUserScope _scope;
-
-    public HttpContextCurrentUserAccessor(IHttpContextAccessor http, IUserScope scope)
-    {
-        _http = http;
-        _scope = scope;
-    }
-
     public Guid UserId
     {
         get
         {
-            var claim = _http.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            var claim = http.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
             if (Guid.TryParse(claim, out var fromCookie)) return fromCookie;
 
-            if (_scope.Current is { } fromScope) return fromScope;
+            if (scope.Current is { } fromScope) return fromScope;
 
             throw new InvalidOperationException(
                 "No user context available. HTTP requests resolve from cookie; " +
