7e9c132c8ae048af4446934bde1e7953b03ecf3c
Marcell Hernández <marcelljesus1218@gmail.com>
2026-05-12 01:29:02 +0200

    docs(stage-7): restore defense-in-depth comment on CanReactivate

--- ProjectCeres/Services/CategoryPolicies.cs
+++ ProjectCeres/Services/CategoryPolicies.cs
@@ -29,5 +29,7 @@ public static class CategoryPolicies
         return Result.Ok();
     }
 
+    /// <summary>System-immutability also blocks reactivate; system categories shouldn't
+    /// be in the archived list in the first place, but this is defense in depth.</summary>
     public static Result CanReactivate(Category category) => CanEdit(category);
 }
